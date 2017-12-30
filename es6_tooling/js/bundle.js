95c3d1960eef427f586313ab84751341ab43ccbf
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-12-30 13:55:19 -0500

    Webpack + Babel tooling

---
+++ es6_tooling/js/bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _npmLodash = require("npm:lodash");
+
+/*!
+	Nicolas M. Pardo
+	ES6 for Dummies
+*/
+console.log((0, _npmLodash.kebabCase)('HELLO_my_FriendsThere'));
+
+window.onload = function () {};
---
+++ es6_tooling/js/main.js
@@ -0,0 +1,14 @@
+/*!
+	Nicolas M. Pardo
+	ES6 for Dummies
+*/
+
+import {
+	kebabCase,
+} from 'npm:lodash';
+
+console.log(kebabCase('HELLO_my_FriendsThere'));
+
+window.onload = () => {
+
+};
